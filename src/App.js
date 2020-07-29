fd54825ae8c070468f4750854a7b26efc94ef1ef
renebreslauer <renebreslauer@me.com>
2020-07-29 11:49:42 -0400

    disable horizontal scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import './App.scss'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { VerticalContainer, HorizontalContainer } from './pages/index'
+import {
+  VerticalContainer,
+  HorizontalContainer,
+  Retention,
+  Growth,
+  Home,
+} from './pages/index'
 import { UpperNav, LowerNav } from './components/index'
-
 
 function App() {
   return (
-  <>
-  <Router>
-  <UpperNav />
-    <Switch>
-      <Route exact path="/">
-        <HorizontalContainer />
-      </Route>
-    </Switch>
-  </Router>
-  </>
+    <>
+      <Router>
+        <UpperNav />
+        <Switch>
+          <Route path="/Growth" component={HorizontalContainer}></Route>
+          <Route path="/Retention" component={HorizontalContainer}></Route>
+          <Route exact path="/" component={HorizontalContainer}></Route>
+        </Switch>
+      </Router>
+    </>
   )
 }
 
-export default App;
+export default App
