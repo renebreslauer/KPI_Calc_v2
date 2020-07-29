451a2a6415d7912b6f981991c24f83cfdf4f8f41
renebreslauer <renebreslauer@me.com>
2020-07-29 01:35:13 -0400

    scrolling columns

--- src/components/ColumnGrid/ColumnGrid.jsx
+++ src/components/ColumnGrid/ColumnGrid.jsx
@@ -3,26 +3,28 @@ import './ColumnGrid.scss'
 import { Column } from '../index'
 
 function ColumnGrid() {
+  const header = [
+    'Rate of Return',
 
-    const Header = [
-       'Rate of Return',
-       'Burn Rate',
-       'Churn Rate',
-       'Net Promoter Score'
-    ]
- 
-  
-    return (
-        <>
-        <div className="column_grid_wrapper">
-        {Header.map((data, index) => (
-        
-            <Column header={data} />
+    'Burn Rate',
+
+    'Churn Rate',
+
+    'Net Promoter Score',
+  ]
+
+  return (
+    <>
+      <div className="column_grid_wrapper">
+        {header.map((data, index) => (
+          <Column
+            header={data}
+            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
         ))}
-    
-        </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default ColumnGrid
