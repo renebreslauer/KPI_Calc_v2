b29c38e29dac365942753891cb8a48216d295516
renebreslauer <renebreslauer@me.com>
2020-07-30 01:55:01 -0400

    conditional rendering columns from data

--- src/components/ColumnGrid/ColumnGrid.jsx
+++ src/components/ColumnGrid/ColumnGrid.jsx
@@ -1,51 +1,33 @@
 import React from 'react'
 import './ColumnGrid.scss'
 import { Column, AboutColumn } from '../index'
-import { Category, Retention, retentionDef } from '../../data'
+import { Category, Retention, Subcategory} from '../../data'
 
-class ColumnGrid extends React.Component{
-
-render() {
-
-
-
+function ColumnGrid() {
     return (
         <>
-        
         <div className="column_grid_wrapper">
-        {retentionDef.map((retentionDefinition,index) => (
-        <AboutColumn key={index} about_column_text={retentionDefinition.meaning} />
-        ))}
-      
-        {Retention.map((retentionData) => (
-        <Column header={retentionData.name}
-        body={retentionData.definition}
-        indicator={retentionData.indicator}
-        benchmark={retentionData.benchmark}
-        moreInfo={retentionData.moreInfo} />
-        ))}
+        {Category.map((categoryDef) => {
+            if (categoryDef.name === "Retention") {
+        return <AboutColumn about_column_text={categoryDef.meaning} />
+            }
+        })}
+
+        {Subcategory.map((subcatData) => {
+            if (subcatData.category === 'retention') {
+        return <Column header={subcatData.name}
+        body={subcatData.definition}
+        indicator={subcatData.indicator}
+        benchmark={subcatData.benchmark}
+        moreInfo={subcatData.moreInfo} />
+            }
+        })}
         </div>
 
         </>
     )
 }
-}
 
-//     <>
-//       <div className="column_grid_wrapper">
-//         <AboutColumn about_column_text="Retention KPI's are all about retaining customers or figuring out why customers are abandoning your funnel." />
-//         {retention.map((retentionData) => (
-//           <Column
-//             header={retentionData.header}
-//             body={retentionData.definition}
-//             indicator={retentionData.indicator}
-//             benchmark={retentionData.benchmark}
-//             moreInfo={retentionData.moreInfo}
-//           />
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
 
 export default ColumnGrid
+
--- src/data.js
+++ src/data.js
@@ -1,28 +1,33 @@
 export const Category =
 [
 	{
+		
 		"name": "Retention",
 		"meaning": "Retention KPIs are all about retaining customers or figuring out why customers are abandoning your funnel."
 	},
 	{
+		
 		"name": "Growth",
 		"meaning": "Really, all of these KPIs are about growth - but these ones are more directly about revenue growth."
 	},
 	{
+		
 		"name": "Ads",
 		"meaning": "These KPIs are all about advertisements."
 	},
 	{
+		
 		"name": "SEO",
 		"meaning": "These KPIs will help you with your social strategy."
 	},
 	{
+	
 		"name": "Social",
 		"meaning": "These are your social media KPIs"
 	}
 ]
 
-export const Retention =
+export const Subcategory =
 [
 
 	{
@@ -30,7 +35,8 @@ export const Retention =
 	"definition": "Simply put, your RVR is how many repeat visitors you have.",
 	"indicator": "Is your content engaging enough? Do customers want to buy more of your products? Returning visitors helps you figure out your strategy across verticals.",
 	"benchmark": "<25% is ideal. If you're over 40%, you're killing it.",
-	"moreInfo": "RVR is definitely one of those 'hidden' KPIs that a ton of marketers like to ignore. But honestly, RVR can give you a good luck at how your overall content strategy is going. Return customers are 5x more valuable than new ones, so a high RVR % is huge."
+	"moreInfo": "RVR is definitely one of those 'hidden' KPIs that a ton of marketers like to ignore. But honestly, RVR can give you a good luck at how your overall content strategy is going. Return customers are 5x more valuable than new ones, so a high RVR % is huge.",
+	"category": "retention"
 	
 	},
 	{
@@ -38,27 +44,41 @@ export const Retention =
 	"definition": "Google defines bounce rate as “a single page session on your site“. Bounce rate is a measurement of how many people are viewing one “page” (landing page, site, blog content, etc.) and then leaving.",
 	"indicator": "Stopping conversion loss. Also, bounce rate can give you some key insights into how you’re layering your site content. If your prospects are leaving after one page, you need to figure out why.",
 	"benchmark": "Depends on the content type.",
-	"moreInfo": "Having a high bounce rate doesn’t have to be a bad thing; it depends on where it’s happening. Landing pages, forms, and pillar content (or any other one-stop-shop) are going to have high bounce rates. But, if your eCommerce site has a high bounce rate, you need to take some action. Why are your customers ditching your funnel mid-dance?"
+	"moreInfo": "Having a high bounce rate doesn’t have to be a bad thing; it depends on where it’s happening. Landing pages, forms, and pillar content (or any other one-stop-shop) are going to have high bounce rates. But, if your eCommerce site has a high bounce rate, you need to take some action. Why are your customers ditching your funnel mid-dance?",
+	"category": "retention"
 	},
 	{
 	"name": "Customer Churn Rate",
 	"definition": "Your churn rate is the percentage of your customers who cancel (or fail to renew) their subscription with your service.",
 	"indicator": "SaaS (or other sub-based) businesses looking to make a profit. If customers are canceling their sub before they hit your typical customer acquisition cost (CAC,) you’ve got a big problem.",
 	"benchmark": "1-7% monthly",
-	"moreInfo": "Churn rate is the perfect spot to start digging around. You need to figure out why customers are leaving. To do this, you can start sending out surveys, looking into customer particulars, analyzing your products, and reviewing competitors in your space. Churn rate can also give you some critical insights into overall market trends."
+	"moreInfo": "Churn rate is the perfect spot to start digging around. You need to figure out why customers are leaving. To do this, you can start sending out surveys, looking into customer particulars, analyzing your products, and reviewing competitors in your space. Churn rate can also give you some critical insights into overall market trends.",
+	"category": "retention"
 	},
 	{
 	"name": "Net Promoter Score",
 	"definition": "Addressing customer concerns, quick-fixing client relationships, finding weak team members, discovering marketing-service gaps.",
 	"benchmark": "Since NPS involves a survey, you probably won't be self-calculating NPS",
-	"moreInfo": "NPS is massive in B2B client interactions. If you can figure out where you’re both not aligning early (first 30 days) you can quickly save the relationships. Don’t just send an NPS survey out at the end of your client relationships, use it as a way to touch base throughout. NPS can be used as a retention hack that can save you some valuable customers. Want to get started? There are plenty of “free trial” NPS services out there that will give you a few for free. "
-	}
-]
-
-export const retentionDef = 
-[
+	"moreInfo": "NPS is massive in B2B client interactions. If you can figure out where you’re both not aligning early (first 30 days) you can quickly save the relationships. Don’t just send an NPS survey out at the end of your client relationships, use it as a way to touch base throughout. NPS can be used as a retention hack that can save you some valuable customers. Want to get started? There are plenty of “free trial” NPS services out there that will give you a few for free.",
+	"category": "retention"
+	},
 	{
-		"meaning":"Retention KPIs are all about retaining customers or figuring out why customers are abandoning your funnel."
+		"name": "Return on Marketing Investment (ROI)",
+		"definition": "ROI is a measure of how much revenue your marketing campaigns achieve. This is the end-game. The be-all-end-all of KPIs.",
+		"indicator": "Analyzing marketing campaigns.",
+		"benchmark": "ROIs vary by industry. Here’s a tool that shows you industry specifics.",
+		"moreInfo": "All of the metrics that follow should be used to grow your ROI. Capturing a high ROI % is the goal of almost all marketing campaigns (save possibly branding.) If you feel like your ROI isn’t as high as it should be, keep reading.",
+		"category": "growth"
+		},
+		{
+			"name": "Customer Lifetime Value",
+			"definition": "A measure of how much money you can expect from a single customer’s account.",
+			"indicator": "Measuring against your CAC to figure out how worthwhile your marketing strategies are. You need to know how fast you’re going to recover your CAC.",
+			"benchmark": "benchmark",
+			"moreInfo": "Measuring your LTV against your CAC is insanely important. You want to know how much your customers are really worth, so that you can determine a good CAC benchmark.",
+			"category": "growth"
+			},
 
-	}
 ]
+
+
